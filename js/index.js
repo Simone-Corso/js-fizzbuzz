9ae59ff833eab4654a050a2e6791fdabbaf00b58
Simone-Corso <scorso26@gmail.com>
2023-11-14 15:58:27 +0100

    nella console.log funziona correttamente il ciclo dei numeri fino a 100

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@
  //* Adesso realizziamo una stampa per la console, i numeri da 1 a 100:
 
  for ( let i = 1; i <= 100 ; i++){
-    
+    console.log(i)
  }
 
     //*const wrapperElement = document.querySelector('my_wrapper')
