3c8c2097c66e79faf37b7ff1eb6fc9293e35baea
Simone-Corso <scorso26@gmail.com>
2023-11-14 15:53:21 +0100

    ho dovuto mettere slesh front nel 9 perchè nell'esercizio richiede solo stampare nella console, quello avviene dopo con il bonus 1

--- js/index.js
+++ js/index.js
@@ -2,3 +2,8 @@
  //per i multipli di 3 stampi “Fizz” al posto del numero
  //per i multipli di 5 stampi “Buzz” al posto del numero
  //per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz” al posto del numero.
+
+
+ //* Adesso realizziamo una stampa per la console, i numeri da 1 a 100:
+
+    //*const wrapperElement = document.querySelector('my_wrapper')
