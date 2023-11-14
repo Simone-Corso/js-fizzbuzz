732a0a8c693f07cfe4815a4babd5012e57069847
Simone-Corso <scorso26@gmail.com>
2023-11-14 17:26:33 +0100

    cambiando l'ordine del if 15%, mi ha dato nella console una corretta ordine

--- js/index.js
+++ js/index.js
@@ -9,7 +9,8 @@
  for ( let i = 1; i <= 100 ; i++){
 
 //per i multipli di 3 stampi “Fizz” al posto del numero
-    if (i % 3 == 0) console.log("Fizz");
+    if (i % 15 == 0) console.log("FizzBuzz");
+    else if (i % 3 == 0) console.log("Fizz");
     else if (i % 5 == 0) console.log("Buzz");
     else console.log(i)
     
